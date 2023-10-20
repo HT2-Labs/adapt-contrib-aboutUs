f33c634970b7a48ae0b3e23c0f7a727ae7c48dd0
Lemmy Adams <lemmy.adams@learningpool.com>
2023-10-20 12:52:35 +0100

    Used tertiary operator on social links

--- templates/aboutUsSocialLinks.jsx
+++ templates/aboutUsSocialLinks.jsx
@@ -8,13 +8,13 @@ export default function AboutUsSocialLinks (props) {
   return (
     <div className='aboutus__sociallinks' role='list'>
       {socialLinks.map((socialLink) => {
-        if (socialLink._link){
-          return (
+        const returnedHtml = !socialLink._link ?  '' : <> {
             <a href={socialLink._link} className={`icon icon-${socialLink._service?.toLowerCase()}`}
               target='_blank' title={`Follow us on ${socialLink._service}`} aria-label={`Follow us on ${socialLink._service}`}>
             </a>
-          )
-        }
+          }
+          </>
+        return returnedHtml
       })}
     </div>
   )
